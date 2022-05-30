9057b9f08ebb3cced603d5146f3f927e0e04bac4
selwa <raphsp@orange.fr>
2022-05-30 19:51:39 +0200

    joining room

--- server.js
+++ server.js
@@ -16,11 +16,46 @@ var io = new socket_io_1.Server(httpServer, {
         methods: ["GET", "POST"]
     }
 });
+var clients = [];
+var cagnottes = [];
+var votesRoom = [];
+function getAllClientsWithSameRoom(room) {
+    return clients.filter(function (e) { return e.room === room; });
+}
 io.on("connection", function (socket) {
-    console.log("new connection");
-    // ...
+    console.log("🟢 new connection");
+    socket.on("test", function (data) {
+        socket.join(data.room);
+        console.log("🧪 test");
+        console.log(data);
+        io.to(data.room).emit("testResponse", {
+            test: "stringtest",
+            socketId: socket,
+            idClient: data.idClient
+        });
+    });
+    socket.on("disconnect", function () {
+        console.log("🔴 user disconnect");
+    });
+    socket.on("joinRoom", function (data) {
+        socket.join(data.room);
+        var newPlayer = {
+            idClient: data.idClient,
+            room: data.room,
+            name: data.name,
+            pts: 0,
+            traitor: false,
+            ptsCagnotte: 0
+        };
+        clients.push(newPlayer);
+        console.log(getAllClientsWithSameRoom(data.room));
+        io.to(data.room).emit("joinRoomResponse", data);
+        io.to(data.room).emit("joinPlayerResponse", {
+            player: newPlayer
+        });
+    });
 });
 var PORT = process.env.port || 6602;
 httpServer.listen(PORT, function () {
-    console.log("🚀server is listening");
+    console.log("🚀 server is listening");
 });
--- server.ts
+++ server.ts
@@ -15,13 +15,62 @@ const io = new Server(httpServer, {
     methods: ["GET", "POST"],
   },
 });
+type Player = {
+  id?: string;
+  idClient: string;
+  room: string;
+  name: string;
+  pts: number;
+  traitor: boolean;
+  ptsCagnotte: number;
+};
+let clients: Player[] = [];
+let cagnottes = [];
+let votesRoom = [];
 
+function getAllClientsWithSameRoom(room) {
+  return clients.filter((e) => e.room === room);
+}
 io.on("connection", (socket) => {
-  console.log("new connection");
-  // ...
+  console.log("🟢 new connection");
+
+  socket.on("test", (data: { room: string; idClient: string }) => {
+    socket.join(data.room);
+    console.log("🧪 test");
+    console.log(data);
+    io.to(data.room).emit("testResponse", {
+      test: "stringtest",
+      socketId: socket,
+      idClient: data.idClient,
+    });
+  });
+
+  socket.on("disconnect", () => {
+    console.log("🔴 user disconnect");
+  });
+  socket.on(
+    "joinRoom",
+    (data: { room: string; name: string; idClient: string }) => {
+      socket.join(data.room);
+      const newPlayer: Player = {
+        idClient: data.idClient,
+        room: data.room,
+        name: data.name,
+        pts: 0,
+        traitor: false,
+        ptsCagnotte: 0,
+      };
+      clients.push(newPlayer);
+      console.log(getAllClientsWithSameRoom(data.room));
+      io.to(data.room).emit("joinRoomResponse", data);
+      io.to(data.room).emit("joinPlayerResponse", {
+        player: newPlayer,
+      });
+    }
+  );
 });
 
 const PORT = process.env.port || 6602;
 httpServer.listen(PORT, () => {
-  console.log("🚀server is listening");
+  console.log("🚀 server is listening");
 });
